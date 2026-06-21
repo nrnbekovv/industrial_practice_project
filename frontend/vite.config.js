0aee9c6abde7619074b0befa3edd0466f9fc8099
nrnbekovv <nrnbekov@gmail.com>
2026-06-21 13:24:44 +0500

    config: update proxy target to railway backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'industrialpracticeproject-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       }
